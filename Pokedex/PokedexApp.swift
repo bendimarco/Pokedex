5d31b445d97c107f7637246baa6aec5eb4ae901e
Ben DiMarco <bendimarco20@gmail.com>
2020-12-05 13:07:52 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ben DiMarco on 12/5/20.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
